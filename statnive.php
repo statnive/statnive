2b4861d47d81d9c516141c6e086130cdbaa8caba
Parhum Khoshbakht <parhum.kh@gmail.com>
2026-04-14 14:40:33 +0200

    release: v0.4.2

--- statnive.php
+++ statnive.php
@@ -3,7 +3,7 @@
  * Plugin Name: Statnive
  * Plugin URI:  https://statnive.com
  * Description: Simple stats, clear decisions. Privacy-first analytics for WordPress.
- * Version:     0.4.1
+ * Version:     0.4.2
  * Requires PHP: 8.0
  * Requires at least: 6.2
  * Author:      Statnive
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin version.
  */
-define( 'STATNIVE_VERSION', '0.4.1' );
+define( 'STATNIVE_VERSION', '0.4.2' );
 
 /**
  * Plugin root directory path.
